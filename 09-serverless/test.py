25749aff6c4bb610c106af859ea5ba3b15b81a0a
Shru <shrutika.gupta@gmail.com>
2025-12-12 11:04:14 -0700

    09 serverless module

---
+++ 09-serverless/test.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+
+
+with open('customer.json', 'r') as f_in:   
+    customer = json.load(f_in)
+    print(customer)
+
+result = requests.post(url, json=customer).json()
+print(result)
